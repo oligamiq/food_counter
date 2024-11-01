623d6128f12fc51e309a3bc4c0b87c5bbdefeb70
oligami <s20032@tokyo.kosen-ac.jp>
2024-11-01 18:13:07 +0900

    initial commit

--- src/app.rs
+++ src/app.rs
@@ -1,176 +1,310 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
-pub struct EguiApp {
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct TemplateApp {
     // Example stuff:
     label: String,
+
+    #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
-    painting: Painting,
+
+    #[serde(skip)]
+    sold_food: Vec<SoldFood>,
 }
 
-impl Default for EguiApp {
+impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
-            painting: Default::default(),
+            sold_food: vec![],
         }
     }
 }
 
-impl egui::app::App for EguiApp {
-    fn name(&self) -> &str {
-        "Egui Template"
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct SoldFood {
+    pub name: String,
+    pub time: chrono::DateTime<chrono::Utc>,
+}
+
+impl TemplateApp {
+    pub fn add_sold_food(&mut self, name: String) {
+        for _ in 0..3 {
+            self.sold_food.push(SoldFood {
+                name: name.clone(),
+                time: chrono::Utc::now(),
+            });
+        }
+
+        // 保存
+        let path = "sold_food.json";
+        if let Err(e) = self.save_to_file(std::path::Path::new(path)) {
+            eprintln!("Failed to save file: {}", e);
+        }
     }
 
-    /// Called by the framework to load old app state (if any).
-    fn load(&mut self, storage: &dyn egui::app::Storage) {
-        *self = egui::app::get_value(storage, egui::app::APP_KEY).unwrap_or_default()
+    // 名前ごとに売れた個数を返す
+    pub fn sold_food_count(&self) -> Vec<(String, usize)> {
+        // プレーン、チョコ、いちご、はちみつ、シナモン
+        let mut count = vec![];
+
+        for food in &["プレーン", "チョコ", "いちご", "はちみつ", "シナモン"] {
+            count.push((
+                food.to_string(),
+                self.sold_food.iter().filter(|f| f.name == *food).count(),
+            ));
+        }
+
+        count
     }
 
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        // if let Some(storage) = cc.storage {
+        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
+
+        let mut fonts = egui::FontDefinitions::default();
+
+        fonts.font_data.insert(
+            "NotoSansJP".to_owned(),
+            egui::FontData::from_static(include_bytes!("../fonts/NotoSansJP-Regular.ttf")),
+        );
+
+        fonts
+            .families
+            .get_mut(&egui::FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "NotoSansJP".to_owned());
+
+        cc.egui_ctx.set_fonts(fonts);
+
+        Default::default()
+    }
+
+    pub fn save_to_file(&self, path: &std::path::Path) -> Result<(), Box<dyn std::error::Error>> {
+        if std::fs::exists(path)? {
+            std::fs::remove_file(path)?;
+        }
+        let file = std::fs::File::create(path)?;
+        serde_json::to_writer(file, self.sold_food.as_slice())?;
+
+        Ok(())
+    }
+
+    pub fn load_from_file(
+        &mut self,
+        path: &std::path::Path,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        if !std::fs::exists(path)? {
+            return Ok(());
+        }
+
+        let file = std::fs::File::open(path)?;
+        self.sold_food = serde_json::from_reader(file)?;
+
+        Ok(())
+    }
+}
+
+impl eframe::App for TemplateApp {
     /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn egui::app::Storage) {
-        egui::app::set_value(storage, egui::app::APP_KEY, self);
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        // eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn ui(&mut self, ctx: &egui::CtxRef, integration_context: &mut egui::app::IntegrationContext) {
-        let EguiApp {
-            label,
-            value,
-            painting,
-        } = self;
-
-        // Examples of how to create differens panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::SidePanel::left("side_panel", 200.0).show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::f32(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
-                ui.add(
-                    egui::Hyperlink::new("https://github.com/emilk/egui/").text("Powered by Egui"),
-                );
-            });
-        });
-
-        egui::TopPanel::top("top_panel").show(ctx, |ui| {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
+
             egui::menu::bar(ui, |ui| {
-                egui::menu::menu(ui, "File", |ui| {
-                    if ui.button("Quit").clicked {
-                        integration_context.output.quit = true;
-                    }
-                });
+                // NOTE: no File->Quit on web pages!
+                let is_web = cfg!(target_arch = "wasm32");
+                if !is_web {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                        // save
+                        let path = "sold_food.json";
+                        if ui.button("Save").clicked() {
+                            if let Err(e) = self.save_to_file(std::path::Path::new(path)) {
+                                eprintln!("Failed to save file: {}", e);
+                            }
+                        }
+                        // load
+                        if ui.button("Load").clicked() {
+                            if let Err(e) = self.load_from_file(std::path::Path::new(path)) {
+                                eprintln!("Failed to load file: {}", e);
+                            }
+                        }
+                    });
+                    ui.add_space(16.0);
+                }
+
+                egui::widgets::global_theme_preference_buttons(ui);
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Egui Template");
-            ui.hyperlink("https://github.com/emilk/egui_template");
-            ui.add(egui::github_link_file_line!(
-                "https://github.com/emilk/egui_template/blob/master/",
-                "Direct link to source code."
-            ));
-            egui::demos::warn_if_debug_build(ui);
+            let range = ui.clip_rect().width();
+            let text_size = range * 0.03;
 
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("sold food counter");
+
+            // ui.horizontal(|ui| {
+            //     ui.label("Write something: ");
+            //     ui.text_edit_singleline(&mut self.label);
+            // });
+
+            // ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            // if ui.button("Increment").clicked() {
+            //     self.value += 1.0;
+            // }
+
+            // ui.separator();
+
+            // ui.add(egui::github_link_file!(
+            //     "https://github.com/emilk/eframe_template/blob/main/",
+            //     "Source code."
+            // ));
+
+            // ui.with_layout(
+            //     egui::Layout::top_down_justified(egui::Align::Center),
+            //     |ui| {
             ui.separator();
 
-            ui.heading("Central Panel");
-            ui.label("The central panel the region left after adding TopPanel's and SidePanel's");
-            ui.label("It is often a great place for big things, like drawings:");
-
-            ui.heading("Draw with your mouse to paint:");
-            painting.ui_control(ui);
-            egui::Frame::dark_canvas(ui.style()).show(ui, |ui| {
-                painting.ui_content(ui);
+            // 大きく真ん中の上に配置
+            // ui.heading("売れた玉数");
+            ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+                ui.label(
+                    egui::RichText::new(format!("{}玉売れました", self.sold_food.len()))
+                        .size(text_size * 2.0),
+                );
             });
-        });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
+            // 下の方
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
+                ui.add_space(text_size * 2.0);
+
+                ui.horizontal(|ui| {
+                    // 0.8倍の範囲にボタンを配置
+                    // プレーン、チョコ、いちご、はちみつ、シナモン
+                    let food_list = &["プレーン", "チョコ", "いちご", "はちみつ", "シナモン"];
+                    let spacing = range * 0.8 / (food_list.len() as f32 + 1.0); // ボタンの間にスペースを加える
+
+                    ui.add_space(range * 0.15); // 左スペースを追加してボタンを中央寄せに
+                    for food in food_list {
+                        let space = spacing - text_size * 4 as f32;
+
+                        ui.add_space(space); // 左スペースを追加してボタンを中央寄せに
+                        if ui
+                            .button(egui::RichText::new(*food).size(text_size))
+                            .clicked()
+                        {
+                            self.add_sold_food(food.to_string());
+                        }
+                    }
+
+                    // 取り消し
+                    ui.add_space(spacing * 0.2); // 左スペースを追加してボタンを中央寄せに
+                    if ui
+                        .button(egui::RichText::new("取り消し").size(text_size))
+                        .clicked()
+                    {
+                        self.sold_food.pop();
+
+                        self.save_to_file(std::path::Path::new("sold_food.json"))
+                            .unwrap_or_else(|e| eprintln!("Failed to save file: {}", e));
+                    }
+                });
             });
-        }
 
-        // Resize the glium window to be just the size we need it to be:
-        integration_context.output.window_size = Some(ctx.used_size());
-    }
-}
+            let binding = self.sold_food_count();
+            let max = binding
+                .iter()
+                .cloned()
+                .map(|(_, count)| count)
+                .max()
+                .unwrap_or(0);
 
-// ----------------------------------------------------------------------------
+            {
+                use egui_plotter::EguiBackend;
+                use plotters::prelude::*;
 
-/// Example code for painting on a canvas with your mouse
-#[derive(Clone, serde::Deserialize, serde::Serialize)]
-struct Painting {
-    lines: Vec<Vec<egui::Vec2>>,
-    stroke: egui::Stroke,
-}
+                ui.scope_builder(
+                    egui::UiBuilder::new().max_rect({
+                        // 中心に向かって0.6倍
+                        let rect = ui.clip_rect();
 
-impl Default for Painting {
-    fn default() -> Self {
-        Self {
-            lines: Default::default(),
-            stroke: egui::Stroke::new(1.0, egui::color::LIGHT_BLUE),
-        }
-    }
-}
+                        // println!("rect: {:?}", rect);
 
-impl Painting {
-    pub fn ui_control(&mut self, ui: &mut egui::Ui) {
-        ui.horizontal(|ui| {
-            self.stroke.ui(ui, "Stroke");
+                        let center = rect.center();
+                        let width = (rect.width() * 0.6).max(200.0);
+                        let height = (rect.height() * 0.6).max(200.0);
+                        let correct = egui::vec2(width, height);
+                        let correct = egui::Rect::from_center_size(center, correct);
+                        correct
+                    }),
+                    |ui| {
+                        let root = EguiBackend::new(ui).into_drawing_area();
+                        let mut chart = ChartBuilder::on(&root)
+                            .margin(5)
+                            .x_label_area_size(0)
+                            .y_label_area_size(30)
+                            .build_cartesian_2d((0u32..4u32).into_segmented(), 0..max + 1)
+                            .unwrap();
+
+                        chart.configure_mesh().draw().unwrap();
+
+                        let sold_food_count = self.sold_food_count();
+
+                        chart
+                            .draw_series(
+                                Histogram::vertical(&chart)
+                                    .style(RED.mix(0.5).filled())
+                                    .data(
+                                        sold_food_count
+                                            .iter()
+                                            .enumerate()
+                                            .map(|(i, (_, count))| (i as u32, *count)),
+                                    ),
+                            )
+                            .unwrap()
+                            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+                        root.present().unwrap();
+                    },
+                );
+            }
+
             ui.separator();
-            if ui.button("Clear Painting").clicked {
-                self.lines.clear();
-            }
         });
     }
+}
 
-    pub fn ui_content(&mut self, ui: &mut egui::Ui) {
-        let painter = ui.allocate_painter(ui.available_size_before_wrap_finite());
-        let rect = painter.clip_rect();
-        let id = ui.make_position_id();
-        let response = ui.interact(rect, id, egui::Sense::drag());
-
-        if self.lines.is_empty() {
-            self.lines.push(vec![]);
-        }
-
-        let current_line = self.lines.last_mut().unwrap();
-
-        if response.active {
-            if let Some(mouse_pos) = ui.input().mouse.pos {
-                let canvas_pos = mouse_pos - rect.min;
-                if current_line.last() != Some(&canvas_pos) {
-                    current_line.push(canvas_pos);
-                }
-            }
-        } else if !current_line.is_empty() {
-            self.lines.push(vec![]);
-        }
-
-        for line in &self.lines {
-            if line.len() >= 2 {
-                let points: Vec<egui::Pos2> = line.iter().map(|p| rect.min + *p).collect();
-                painter.add(egui::PaintCmd::line(points, self.stroke));
-            }
-        }
-    }
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,4 @@
-#![forbid(unsafe_code)]
-#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
-#![warn(clippy::all)]
+#![warn(clippy::all, rust_2018_idioms)]
 
-pub mod app;
-pub use app::EguiApp;
-
-// ----------------------------------------------------------------------------
-// When compiling for web:
-
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-
-/// This is the entry-point for all the web-assembly.
-/// This is called once from the HTML.
-/// It loads the app, installs some callbacks, then returns.
-/// You can add more callbacks like this if you want to call in to your code.
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-pub fn start(canvas_id: &str) -> Result<(), wasm_bindgen::JsValue> {
-    let app = EguiApp::default();
-    egui_web::start(canvas_id, Box::new(app))?;
-    Ok(())
-}
+mod app;
+pub use app::TemplateApp;
